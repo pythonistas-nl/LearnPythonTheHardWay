843f4a9a134f85aa303c2f3ffc62b675d1118f9a
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-05-26 23:33:45 +0200

    Add.

---
+++ exc_1/exc_1_vince-nieuw.py
@@ -0,0 +1,9 @@
+# We should probably just skip this one.
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
