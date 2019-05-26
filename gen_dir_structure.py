f54fe60269663a47dc601d457953b89235a7ea53
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-05-26 23:27:45 +0200

    spelling :)

--- gen_dir_structure.py
+++ gen_dir_structure.py
@@ -1,4 +1,4 @@
-#Generates all 52 folders needed for the excersises in the book. 
+#Generates all 52 folders needed for the exercises in the book. 
 #if you want to delete all folders again, run the command "rm -rf exc_*")
 #All content in that folders will also be gone. 
 
