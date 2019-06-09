057b80f02f5ff9ff0dae64a061b26bef8645e37d
Vince <Vince-Nieuw@github.localhost>
2019-06-09 21:10:50 +0200

    Added exc 2

---
+++ exc_2/exc_2_vince-nieuw.py
@@ -0,0 +1,20 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+# In Pythonistas-NL, we usually refer to the # character as a pound or hastag character.
+
+# I want to clarify one thing, there are two ways to comment out multiple lines:
+# The first way is to 
+# put a hasthag before each line
+# But alternatively
+""" 
+You can also use 3x double quotes to start something called a docstring.
+We'll cover this in more depth later
+"""
