6a0d113b6344adb99d7ea0853d70c8e40b51d8a2
Vince <Vince-Nieuw@github.localhost>
2019-06-09 21:29:47 +0200

    Added explanation

--- exc_3/exc_3_vince-nieuw.py
+++ exc_3/exc_3_vince-nieuw.py
@@ -1,22 +1,47 @@
+# Prints a single line of text. 
 print("I will now count my chickens:" )
+
 #
+# Print the string "Hens" , then prints the result of 25 + (30 / 6) . Python respects the order of 
+# mathemetical calculation as we know it. 
 print("Hens", 25 + 30 / 6) 
+
+# Prints the string "Roosters", then prints the result of 100 - (25 * 3 % 4). 
 print("Roosters", 100 - 25 * 3 % 4 )
 #
+# Prints the below sentence.
 print(" Now I will count the eggs:")
 #
+# Prints only the sentence. Does NOT actually calculate anyhthing here. 
 print("Is it true that (3 + 2 ) < ( 5 - 7 )")
 #
+# The actual calculation is done below. The result is printed on a new line. 
 print( 3 + 2 < 5 - 7 ) 
 #
+# Prints the string "What is 3 + 2?". It then prints the outcome of the sum. 
+# This is initiated by the calculation we provide after the comma.
 print("What is 3 + 2?" , 3 + 2) 
+
+# Same as previous line. 
 print(" What is 5 - 7?", 5 - 7)
-#
+
+# Prints the sentence "Oh, that's why it's False.") 
 print("Oh, that's why it's False.") # Typing False with capital F may cause some IDE's to give it a wrong color. 
-#
+
+# Prints the sentence "How about some more".
 print("How about some more.")
-#
+
+# Prints the sentence "Is it greater" , then prints "True" on thesame line. This is because it is true that 5 > -2.
 print("Is it greater" , 5 > -2)
+
+# Here they added the '='  sign in the comparrision. This means that if the number is equal, it is also true.
+# Try it:
+"""
+3 > 3  ---> Will be false
+3 >= 3  --> Will be true.
+"""
+
+# Explanation provided in above docstring.
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
 
