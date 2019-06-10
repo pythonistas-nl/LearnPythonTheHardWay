884f09394e88aaab758ec8d4c31a85268bb3559a
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 16:11:19 +0200

    Added comments explaning the  code.

--- 6. Strings and Text/exc_6_vince-nieuw.py
+++ 6. Strings and Text/exc_6_vince-nieuw.py
@@ -1,20 +1,35 @@
+# Create a variable with name types_of_people, the variable holds an integer value of 10. 
 types_of_people = 10
+
+# Create a variable (x), in that variable we store the formatted string of: "There are 10 types of people."
+# The string only gets formatted since we're creating it with a f(ormat) flag. 
 x = f"There are {types_of_people} types of people."
 
+# Create a variable (binary) that holds the value/string 'binary'.
 binary = "binary"
+
+# Create a variable (do_not) that holds the value/string 'don't'.
 do_not = "don't"
+
+# Create a variable (y), in that, store the following formatted string:
+# "Those who know binary and those who don't."
 y = f"Those who know {binary} and those who {do_not}."
 
+# Print variable x. 
 print(x)
+
+# Print variable y.
 print(y)
 
+# Print "I said:", then print the complete variable x.
 print(f"I said: {x}")
+# Print "I said:", then print the complete variable y.
 print(F"I also said: '{y}'")
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
 
-print(joke_evaluation).format(hilarious))
+print(joke_evaluation.format(hilarious))
 
 w = "This is the left side of ..."
 e = "a string with a right side."
