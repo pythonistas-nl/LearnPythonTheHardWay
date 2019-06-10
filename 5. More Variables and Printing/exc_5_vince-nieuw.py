ea1885a80d6cd8e5dc930a9d98a54522f778e3ab
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 14:23:49 +0200

    Changed values.

--- 5. More Variables and Printing/exc_5_vince-nieuw.py
+++ 5. More Variables and Printing/exc_5_vince-nieuw.py
@@ -1,10 +1,10 @@
 my_name = 'Vincent Nieuwenburg'
 my_age = 128 # definitely a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
+my_height = 89 # inches
+my_weight = 183 # lbs
+my_eyes = 'Green'
 my_teeth = 'White'
-my_hair = 'Brown'
+my_hair = 'Blonde'
 
 print(f"Let's talk about {my_name}.")
 print(f"He's {my_height} inches tall.")
@@ -14,7 +14,7 @@ print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
 # This line is tricky, try to get it exactly right.
-total = my_age + my_heigth + my_weight
+total = my_age + my_height + my_weight
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
 
 
