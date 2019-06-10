ac7044906f74e0464bd8dab9f75787696767baa2
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 14:25:38 +0200

    Removed "my_" prefix from all vars.

--- 5. More Variables and Printing/exc_5_vince-nieuw.py
+++ 5. More Variables and Printing/exc_5_vince-nieuw.py
@@ -1,18 +1,18 @@
-my_name = 'Vincent Nieuwenburg'
-my_age = 128 # definitely a lie
-my_height = 89 # inches
-my_weight = 183 # lbs
-my_eyes = 'Green'
-my_teeth = 'White'
-my_hair = 'Blonde'
+name = 'Vincent Nieuwenburg'
+age = 128 # definitely a lie
+height = 89 # inches
+weight = 183 # lbs
+eyes = 'Green'
+teeth = 'White'
+hair = 'Blonde'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # This line is tricky, try to get it exactly right.
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
