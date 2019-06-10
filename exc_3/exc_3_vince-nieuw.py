255e55220ad2abe9384de41a3bd5bb30055e7211
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 13:23:56 +0200

    Rewritten for floats and spelling.

--- exc_3/exc_3_vince-nieuw.py
+++ exc_3/exc_3_vince-nieuw.py
@@ -1,40 +1,44 @@
+"""
+Author: Vince-Nieuw
+Version: 2.0
+Comment: Rewritten for use with floats.
+"""
+
 # Prints a single line of text. 
 print("I will now count my chickens:" )
 
-#
-# Print the string "Hens" , then prints the result of 25 + (30 / 6) . Python respects the order of 
-# mathemetical calculation as we know it. 
-print("Hens", 25 + 30 / 6) 
+# Print the string "Hens" , then prints the result of 25 + (30 / 6) . 
+# Python respects the order of  mathematical calculation as we know it. 
+print("Hens", 25.0 + 30.0 / 6.0) 
 
 # Prints the string "Roosters", then prints the result of 100 - (25 * 3 % 4). 
-print("Roosters", 100 - 25 * 3 % 4 )
-#
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0 )
+
 # Prints the below sentence.
 print(" Now I will count the eggs:")
 #
-# Prints only the sentence. Does NOT actually calculate anyhthing here. 
-print("Is it true that (3 + 2 ) < ( 5 - 7 )")
+# Prints only the sentence. Does NOT actually calculate anything here. 
+print("Is it true that (3.0 + 2.0 ) < ( 5.0 - 7.0 )")
 #
 # The actual calculation is done below. The result is printed on a new line. 
-print( 3 + 2 < 5 - 7 ) 
+print( 3.0 + 2.0 < 5.0 - 7.0 ) 
 #
 # Prints the string "What is 3 + 2?". It then prints the outcome of the sum. 
 # This is initiated by the calculation we provide after the comma.
-print("What is 3 + 2?" , 3 + 2) 
+print("What is 3 + 2?" , 3.0 + 2.0) 
 
 # Same as previous line. 
-print(" What is 5 - 7?", 5 - 7)
+print(" What is 5 - 7?", 5.0 - 7.0)
 
-# Prints the sentence "Oh, that's why it's False.") 
-print("Oh, that's why it's False.") # Typing False with capital F may cause some IDE's to give it a wrong color. 
+# Prints the sentence "Oh, that's why it's False." 
 
 # Prints the sentence "How about some more".
 print("How about some more.")
 
-# Prints the sentence "Is it greater" , then prints "True" on thesame line. This is because it is true that 5 > -2.
-print("Is it greater" , 5 > -2)
+# Prints the sentence "Is it greater" , then prints "True" on the same line. This is because it is true that 5 > -2.
+print("Is it greater" , 5.0 > -2.0)
 
-# Here they added the '='  sign in the comparrision. This means that if the number is equal, it is also true.
+# Here they added the '='  sign in the comparison. This means that if the number is equal, it is also true.
 # Try it:
 """
 3 > 3  ---> Will be false
@@ -42,8 +46,8 @@ print("Is it greater" , 5 > -2)
 """
 
 # Explanation provided in above docstring.
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
 
 
 
