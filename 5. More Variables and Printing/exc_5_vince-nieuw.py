80df59518331ccc9c8a430ae291320a7e8535e4a
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 14:33:34 +0200

    Added retarted to non-retarded metrics conversion.

--- 5. More Variables and Printing/exc_5_vince-nieuw.py
+++ 5. More Variables and Printing/exc_5_vince-nieuw.py
@@ -5,10 +5,12 @@ weight = 183 # lbs
 eyes = 'Green'
 teeth = 'White'
 hair = 'Blonde'
+height_cm = height * 2.54
+weight_kg = weight * 0,453592
 
 print(f"Let's talk about {name}.")
-print(f"He's {height} inches tall.")
-print(f"He's {weight} pounds heavy.")
+print(f"He's {height} inches tall, that's {height_cm} in normal metrics.")
+print(f"He's {weight} pounds heavy, that's {weight_kg} in normal metrics.")
 print("Actually that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
