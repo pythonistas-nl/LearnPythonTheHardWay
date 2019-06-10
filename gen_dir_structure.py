ed36d74b9b2ad17e4ae8a575e6ee6546de4fb550
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 13:26:30 +0200

    Rename

--- gen_dir_structure.py
+++
@@ -1,8 +0,0 @@
-#Generates all 52 folders needed for the exercises in the book. 
-#if you want to delete all folders again, run the command "rm -rf exc_*")
-#All content in that folders will also be gone. 
-
-import os
-
-for num in range (0, 53):
-  os.system("mkdir exc_" + str(num))
