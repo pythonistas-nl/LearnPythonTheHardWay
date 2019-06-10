e680f8e5587b295814788f886f9de0723f3f8646
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 14:24:25 +0200

    Removed empty lines

--- 5. More Variables and Printing/exc_5_vince-nieuw.py
+++ 5. More Variables and Printing/exc_5_vince-nieuw.py
@@ -16,6 +16,3 @@ print(f"His teeth are usually {my_teeth} depending on the coffee.")
 # This line is tricky, try to get it exactly right.
 total = my_age + my_height + my_weight
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
-
-
-
