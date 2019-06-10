72f160a8d9cf3e7089144357bff726861db929a3
Vince Nieuwenburg <vince@MacBook-Pro-Vince.local>
2019-06-10 14:10:34 +0200

    First submission.

---
+++ 5. More Variables and Printing/exc_5_vince-nieuw.py
@@ -0,0 +1,21 @@
+my_name = 'Vincent Nieuwenburg'
+my_age = 128 # definitely a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right.
+total = my_age + my_heigth + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+
+
